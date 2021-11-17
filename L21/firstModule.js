17eb0db448791d96deeee3fb05deff8bf9ca56f8
shauryagupta30 <41203937+shauryagupta30@users.noreply.github.com>
2021-11-17 15:01:40 +0530

    final

--- L21/firstModule.js
+++ L21/firstModule.js
@@ -16,9 +16,7 @@ else{
 console.log(args);
 //node ./firstModule.js -x 4 -y 10
 //node ./firstModule.js --x=4 --y="Shaurya"
-=======
 let parser = require('minimist');
 let args = parser(process.argv);
 
 console.log(args);
->>>>>>> 38eb699456110b9e0cabaff4deb1af1797c184d2
