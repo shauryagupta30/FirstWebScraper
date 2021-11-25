f126c651b30062727fed3cc15dd407f3d7a54972
shauryagupta30 <41203937+shauryagupta30@users.noreply.github.com>
2021-11-26 02:02:27 +0530

    Made Folder

---
+++ L28/firstFolderCreate.js
@@ -0,0 +1,21 @@
+//node firstFolderCreate.js --source=teams.json --dest=root 
+let minimist = require("minimist");
+let fs = require("fs");
+let excel = require("excel4node");
+let path  = require("path");//make folder path notation OS independent
+
+let args = minimist(process.argv);
+
+//this is the string[only string operation possible]
+let teamsJSON = fs.readFileSync(args.source,"utf-8");
+
+//this is the JSON for manipulating the data
+let teams = JSON.parse(teamsJSON);
+
+for(let i = 0;i<teams.length;i++)
+{
+    let folderName = path.join(args.dest,teams[i].name);
+    fs.mkdirSync(folderName);//this makes folder(s)
+}
+
+
