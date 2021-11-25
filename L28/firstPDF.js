23b833bb87486d7c564ad6edc03d12c12d7776fa
shauryagupta30 <41203937+shauryagupta30@users.noreply.github.com>
2021-11-26 02:46:24 +0530

    PDF generated successfully

---
+++ L28/firstPDF.js
@@ -0,0 +1,20 @@
+//node firstPDF.js --source=teams1.json --dest=root
+let minimist = require("minimist");
+let fs = require("fs");
+let pdf = require("pdf-lib");
+let path = require("path");//make folder path notation OS independent
+
+let args = minimist(process.argv);
+
+let teamsJSON = fs.readFileSync(args.source,"utf-8");
+let teams = JSON.parse(teamsJSON);
+
+for(let i = 0;i<teams.length;i++)
+{
+    let teamFolder = path.join(args.dest,teams[i].name);
+    for(let j = 0;j < teams[i].matches.length;j++)
+    {
+        let fileName = path.join(teamFolder,teams[i].matches[j].opponent + ".pdf");
+        fs.writeFileSync(fileName,"","utf-8");
+    }
+}
