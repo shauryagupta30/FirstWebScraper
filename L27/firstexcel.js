86bf0f93852861508903d08520369ee200c3ed0e
shauryagupta30 <41203937+shauryagupta30@users.noreply.github.com>
2021-11-25 17:53:22 +0530

    excel first demo complete

--- L27/firstexcel.js
+++ L27/firstexcel.js
@@ -4,6 +4,7 @@
 let minimist = require("minimist");
 let fs = require("fs");
 let excel = require("excel4node");
+const { FORMERR } = require("dns");
 
 let args = minimist(process.argv);
 console.log(args.source);
@@ -13,11 +14,31 @@ let teams = JSON.parse(teamsJSON);
 
 
 let wb = new excel.Workbook();
+let hs = wb.createStyle({
+    font:{
+        color:"red",
+    }
+})
 for(let i = 0;i<teams.length;i++)
 {
-    wb.addWorksheet(teams[i].name);
+    let sheet = wb.addWorksheet(teams[i].name);
+    sheet.cell(1,1).string("Opponent");
+    sheet.cell(1,2).string("result"); 
+
+    //FOR EACH TEAM 
+    for(let j = 0;j<teams[i].matches.length;j++)
+    {
+        let vs = teams[i].matches[j].opponent;
+        let result = teams[i].matches[j].result;
+        //console.log(result);
+        sheet.cell(j+2,1).string(vs);
+        sheet.cell(j+2,2).string(result);
+    }
+
 }
 wb.write(args.dest);
 
 // console.log(teams);
 // console.log(teamsJSON);
+
+//worksheet ke andar result place karn ehai
